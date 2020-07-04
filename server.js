7cc7811a12da64d7308b0564dd1a4dc333088173
MarkGaunt1 <marklgaunt@googlemail.com>
2020-07-04 18:38:32 +0100

    icecream data get/post req with filter & found

--- server.js
+++ server.js
@@ -2,51 +2,60 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
-const mockUserData=[
-    {name:'Mark'},
-    {name:'Jill'}
-    ]
-    app.get('/users', function(req,res){
-         res.json({
-              success: true,
-              message: 'successfully got users. Nice!',
-              users: mockUserData
-         })
-    })
-   
-    app.get('/users/:id',function(req,res){
-        console.log(req.params.id)
-        res.json({
-            success: true,
-            message: 'got one user',
-            user: req.params.id
-        })
-    })
-
-    app.post('/login',function(req,res){
-        const username=req.body.username;
-        const password=req.body.password;
+const myIces = [
+     {name: 'strawberry', supplier: 'Mr Moos', vegan: 'Y'},
+	{name: 'vanilla', supplier: 'Devon Ices',vegan:'Y'},
+	{name: 'chocolate', supplier: 'Devon Ices',vegan:'N'},
+	{name: 'Raspberry', supplier: 'Mr Moos', vegan:'Y'}
+];
+        
+     app.get('/users', function(req,res){
+          res.json({
+               success: true,
+               message: 'Here is all our icecream data!',
+               users: myIces
+          })
+     })
     
-        const mockUsername="billyTheKid";
-        const mockPassword="superSecret";
-    
-        if (username===mockUsername && password===mockPassword){
-             res.json({
-                  success: true,
-                  message: 'password and username match!',
-                  token: 'encrypted token goes here'
-             })
-        } else {
-             res.json({
-                  success: false,
-                  message: 'password and username do not match'
-             })
-        }
-   })
+     app.get('/users/:name',function(req,res){
+          var whatever = req.params.name;
+          var iceCream = myIces.filter(function (e) {
+               console.log(whatever);
+               return e.name === whatever;
+               
+             });
+               res.json({
+               success: true,
+               message: 'Here is your preferred icecream!',
+               user: iceCream
+          })
+     })
 
+     app.post('/login',function(req,res){
 
-app.listen(8000,function(){
+          const found = myIces.find(e => e.name === req.body.name);
+          console.log(found);
+
+          if ('name' in req.body && 'supplier' in req.body && 'vegan' in req.body && !found ) {
+          console.log(req.body);
+          myIces.push(req.body);
+          res.json({
+               success: true,
+               message: 'Delivered, thanks!'
+          })
+     }    else {
+		res.json({
+			success: false,
+			message: !found ? 'Data missing. Please try again ' : 'Icecream already exists!'
+		})
+	}
+     });
+          
+
+ 
+app.listen(8007,function(){
     console.log("server is running")
     })
+
